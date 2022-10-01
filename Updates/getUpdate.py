03434e95308a76a3fd4e8cf676077c0e53a9df53
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2022-10-02 05:10:58 +0800

    test

--- Updates/getUpdate.py
+++ Updates/getUpdate.py
@@ -1,5 +1,13 @@
 from git import Repo
-
+import distutils.dir_util as dUtil
 
 def getUpdateDefault():
-    Repo.clone_from()
+    # todo chang to env
+    Repo.clone_from("https://github.com/JustinDuDuDuDu29/cleaning_robot.git", "./tmp/Update")
+
+def override():
+    dUtil.copy_tree("./tmp/Update", ".")
+
+# getUpdateDefault()
+override()
+# NMSL
